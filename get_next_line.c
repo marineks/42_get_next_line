919ce1364aa6d902bb65884d344baf1a747d0777
Marine SANJUAN <marine@sanjuan.eu>
2021-06-30 18:40:27 +0200

    lines displayed but bug to be fixed with the last line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 11:19:31 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/06/29 20:18:41 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/06/30 18:34:01 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@ char *ft_get_the_line(char *stock) // If "Salut\nc'est moi", expects malloc size
 	int i;
 
 	i = 0;
+	if (stock[i] == '\n' && stock[i + 1])
+	{
+		line = ft_strdup("\n");
+		return (line);
+	}
 	while (stock[i] != '\n')
 		i++;    					//nb: compte 1 de plus donc pas besoin de rajouter 1 au malloc
-	line = (char *)malloc(sizeof(char) * i);
+	line = (char *)malloc(sizeof(char) * (i + 1)); // je sais pas pourquoi mais qd je rajoute + 1 ca marche?
 	if (!line)
 		return (NULL);
-	ft_strlcpy(line, stock, i); // char *dst, const char *src, size_t dstsize
+	ft_strlcpy(line, stock, (i + 1)); // je sais pas pourquoi mais qd je rajoute + 1 ca marche?
 	return (line);
 }
 
@@ -35,11 +40,11 @@ char *ft_get_the_spare(char *stock) // If "Salut\nC'est moi", expects malloc siz
 	int start;
 
 	i = 0;
-	while (stock[i] != '\n') // Trouver le \n
+	while (stock[i] != '\n') 						// Trouver le \n
 		i++;    
-	start = i;
+	start = i + 1; 									// on passe le \n avec +1
 	j = 0;
-	while (stock[i + 1]) // on passe le \n avec +1
+	while (stock[i + 1]) 							// on passe le \n avec +1
 	{
 		i++;
 		j++;
@@ -47,56 +52,60 @@ char *ft_get_the_spare(char *stock) // If "Salut\nC'est moi", expects malloc siz
 	spare = (char *)malloc(sizeof(char) * (j + 1));
 	if (!spare)
 		return (NULL);
-	spare = ft_substr(stock, start, j); // char const *s, unsigned int start, size_t len
+	spare = ft_substr(stock, start, (j + 1));
 	return (spare);
 }
 
 int get_next_line(int fd, char **line)
 {
 	char buffer[BUFFER_SIZE + 1];
-	static char *stock;
+	static char *stock = NULL; 
 	char *tmp;
 	ssize_t ret;
-
-	if (!fd || !line || BUFFER_SIZE == -1)
-		return (-1);
-	ret = read(fd, buffer, BUFFER_SIZE);                                        // expected : cec
-	if (!ret)
-		return (-1);                                                            // UNE ERREUR EST SURVENUE
-	if (ret > 0)
-	{
-			buffer[ret] = '\0';
-			printf("Petit buffer : |%s|\n", buffer);                            // 1st loop : buffer is "Ce\0"
-			tmp = strdup(buffer); 
-			stock = ft_strdup(tmp);
-			free(tmp);
-			printf("Stock 1 : |%s|\n", stock);
-			ret = read(fd, buffer, BUFFER_SIZE);		// i'\n'e
-			while (ft_strchr(stock, '\n') == NULL)
-			{ 
-				         
-				tmp = strdup(buffer);                              
-				printf("Tmp 1 : |%s|\n", tmp);
-				
-				stock = ft_strjoin(stock, tmp);		 // ceci \n e
-				printf("Ca a ete join : |%s|\n", stock);
-				free(tmp);
-			
-				ret = read(fd, buffer, BUFFER_SIZE);  
-			
-			}
-			// *line = stock;
-			*line = ft_get_the_line(stock);
-			printf("Line : |%s|\n", *line);
-			stock = ft_get_the_spare(stock);
-			printf("Stock Spare : |%s|\n", stock);
-			printf("\n");                             
-			return (1);                                                         // UNE LIGNE A ETE LUE
-		}
-		// ret = read(fd, buffer, BUFFER_SIZE);                                 // on relit pour avancer l'offset
-		if (!ret)
-				return (-1);                                                    // UNE ERREUR EST SURVENUE
 	
+	printf("\n");
+	printf("\n");
+	printf("DEBUT FONCTION \n");
+	printf("\n");
+	// printf("Stock 1 : |%s|\n", stock);
+	ret = 0;
+	if (!fd || !line || BUFFER_SIZE == -1)
+		return (-1);					
+	buffer[ret] = '\0';
+	// printf("1 Statut buffer : |%s|\n", buffer);        
+	if (stock == NULL)
+	{
+		ret = read(fd, buffer, BUFFER_SIZE);   
+		if (!ret)
+			return (-1);  
+		tmp = strdup(buffer); 
+		stock = ft_strdup(tmp);
+		free(tmp);
+		// printf("Stock 1 : |%s|\n", stock);
+	}											
+	// if (ret > 0)
+	// {	
+		while (ft_strchr(stock, '\n') == NULL)
+		{ 
+			ret = read(fd, buffer, BUFFER_SIZE); // i'\n'e
+			// printf("2 Statut buffer : |%s|\n", buffer);
+			if (!ret)
+				return (-1);	
+			tmp = strdup(buffer);                              
+			// printf("Tmp 1 : |%s|\n", tmp);
+					
+			stock = ft_strjoin(stock, tmp);		 // ceci \n e
+			// printf("Ca a ete join : |%s|\n", stock);
+			free(tmp);
+	
+		}
+	*line = ft_get_the_line(stock);
+	printf("Line : |%s|\n", *line);
+	stock = ft_get_the_spare(stock);
+	printf("Stock Spare : |%s|\n", stock);
+	printf("\n");                             
+		return (1); 
+	// }                                                                                               
    return (0);                                                                  // END OF FILE REACHED
 }
 
@@ -110,12 +119,22 @@ int main()
 	
 	printf("%d\n", get_next_line(fd, &test));
 	printf("|%s|\n", test);
-	// get_next_line(fd, &test);
-	// printf("|%s|\n", test);
-	// get_next_line(fd, &test);
-	// printf("|%s|\n", test);
-	// get_next_line(fd, &test);
-	// printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
+	get_next_line(fd, &test);
+	printf("|%s|\n", test);
 	// get_next_line(fd, &test);
 	// printf("|%s|\n", test);
 	free(test);
